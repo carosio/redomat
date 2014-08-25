085494383e8a5b53e30841489424c93eef8bee8b
Willy Failla <wfailla@wfailla.de>
2014-08-25 16:38:05 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+set -e
+
 DIRS=$(ls -1 | grep [0-9])
+rc=0
 
 dock() {
 	if [ -e /usr/bin/docker ]; then
@@ -10,11 +13,20 @@ dock() {
 	fi
 }
 
+function build-image() {
+	if ! dock build -t $1 $1; then
+		rc=$?
+		echo "non zero exito status from docker build -t $1 $1: $rc"
+	fi
+}
+
 for x in $DIRS
 do
 	echo
 	echo "starting to create image from $x"
 	echo
 	[ -x $x/setup.sh ] && $x/setup.sh
-	dock build -t $x $x
+	build-image $x
 done
+
+exit $rc

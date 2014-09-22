2363cabb54101a6a35f8dde5e3f88347dd6593ab
Willy Failla <wf@travelping.com>
2014-09-22 17:27:25 +0200

    miner notes

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 test -x ./build.sh
 test -d ./001-ubuntu
 
+TAG=@datum@
 rc=0
 
 dock() {
@@ -16,14 +17,14 @@ dock() {
 }
 
 function build-image() {
-	if ! dock build -t $1 $1; then
+	if ! dock build -t $TAG-$1 $TAG-$1; then
 		rc=1
 		echo "non zero exito status from docker build -t $1 $1: $rc"
 	fi
 }
 
 function run-image() {
-	if ! (docker.io run --privileged=true -i --name=$1 $1 -c '/build/build_tplino-core.sh' && dock commit $1 $1) ; then
+	if ! (docker.io run --privileged=true -i --name=$TAG-$1 $TAG-$1 -c '/build/build_tplino-core.sh' && dock commit $TAG-$1 $TAG-$1) ; then
 		rc=1
 		echo "non zero exito status from docker run --rm -t -i --name=$1 $1: $rc"
 	fi

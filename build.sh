8ef72b4b81d09e33643955742896486dd05e39c8
Willy Failla <wf@travelping.com>
2014-09-23 16:07:22 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -5,20 +5,10 @@ set -e
 test -x ./build.sh
 test -d ./001-ubuntu
 
-source ../docker_api.sh
-
-IFS=$'\n'
-TAG=$USER
-rc=0
-
 for stage in [0-9][0-9][0-9]-*
 do
-	[ ! -e $stage/Dockerfile ] && exit 1
-	DOCKERFILE=$(cat $stage/Dockerfile)
-	for LINE in $DOCKERFILE
-	do
-		$LINE
-	done
+	[ ! -x $stage/build.sh ] && exit 1
+	$stage/build.sh
 done
 
 exit $rc

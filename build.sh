30f7cda49afe808c9fbbe3562c76097b593fb224
Willy Failla <wf@travelping.com>
2014-09-29 12:14:16 +0200

    fixing some typos

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ do
 	[ ! -x $stage/Dockerfile.sh ] && echo "$stage/Dockerfile.sh is dose not exist or is not executable" && exit 1
 	cd $stage
 	STAGE=$(grep "^\s*STAGE=" ./Dockerfile.sh | cut -d= -f2-)
-	[ ! -z SRAGE ] && echo "STAGE variable not set" && exit 1
+	[ -z $STAGE ] && echo "STAGE variable not set" && exit 1
 	echo "starting stage $STAGE"
 	./Dockerfile.sh
 	SQUASH
--- docker_api.sh
+++ docker_api.sh
@@ -11,11 +11,12 @@ FINAL_IMAGE=${BUILDID}-${STAGE}-end_of_stage
 LAST_IMAGE=${BUILDID}-${LASTSTAGE}-end_of_stage
 
 function FROM() {
+	[ -z $1 ] && echo "The RUN command needs at leased one argument" && exit 1
 	IMAGE=$1
 
-	[ ! -z $IMAGE ] && echo "IMAGE variable not set" && exit 1
-	[ ! -z $LAST_IMAGE ] && echo "LAST_IMAGE variable not set" && exit 1
-	[ ! -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
+	[ -z $IMAGE ] && echo "IMAGE variable not set" && exit 1
+	[ -z $LAST_IMAGE ] && echo "LAST_IMAGE variable not set" && exit 1
+	[ -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
 
 	if [ ${IMAGE}="_PREVIOUS" ]; then
 		IMAGE=$LAST_IMAGE
@@ -27,9 +28,9 @@ function ADD() {
 	FILE=$1
 	TARGET=$2
 
-	[ ! -z $LOCATION ] && echo "LOCATION variable not set" && exit 1
-	[ ! -z $FILE ] && echo "FILE variable not set" && exit 1
-	[ ! -z $TARGET ] && echo "TARGET variable not set" && exit 1
+	[ -z $LOCATION ] && echo "LOCATION variable not set" && exit 1
+	[ -z $FILE ] && echo "FILE variable not set" && exit 1
+	[ -z $TARGET ] && echo "TARGET variable not set" && exit 1
 
 	RUN --volume="$LOCATION:/files" "test -d $TARGET || mkdir -p $TARGET && cp -v -r /files/$FILE $TARGET"
 }
@@ -40,9 +41,9 @@ function RUNP() {
 
 function RUN() {
 
-	[ ! -z $1 ]i && echo "The RUN command need at leased one argument" && exit 1
-	[ ! -z $CONTAINER ] && echo "CONTAINER variable not set" && exit 1
-	[ ! -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
+	[ -z $1 ] && echo "The RUN command needs at leased one argument" && exit 1
+	[ -z $CONTAINER ] && echo "CONTAINER variable not set" && exit 1
+	[ -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
 
 	docker_run_args=""
 	#read first char see if it is an -
@@ -63,8 +64,8 @@ function ENV() {
 }
 
 function SQUASH() {
-	[ ! -z $CONTAINER ] && echo "CONTAINER variable not set" && exit 1
-	[ ! -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
+	[ -z $CONTAINER ] && echo "CONTAINER variable not set" && exit 1
+	[ -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
 
 	docker run --name=$CONTAINER $INTER_IMAGE echo "exporting docker image"
 	docker export $CONTAINER | docker import - $INTER_IMAGE
@@ -72,10 +73,10 @@ function SQUASH() {
 
 function _ENDSTAGE
 {
-	[ ! -z $FINAL_IAMGE ] && echo "FINAL_IAMGE variable not set" && exit 1
-	[ ! -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
+	[ -z $FINAL_IAMGE ] && echo "FINAL_IAMGE variable not set" && exit 1
+	[ -z $INTER_IMAGE ] && echo "INTER_IMAGE variable not set" && exit 1
 
-	[ ! -z $(docker instpect $INTER_IMAGE) ] && echo "no container to finalize the image from" && exit 1
+	[ -z $(docker instpect $INTER_IMAGE) ] && echo "no container to finalize the image from" && exit 1
 	docker tag $INTER_IMAGE $FINAL_IMAGE
 }
 

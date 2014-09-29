e6bf7221a6649b0870bd63e3d22c881dd35cc977
Willy Failla <wf@travelping.com>
2014-09-29 11:48:53 +0200

    update outer build script

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-test -x ./build.sh
-test -d ./001-ubuntu
+[ ! -x build.sh ] && echo "build.sh is dose not exist or is not executable" && exit 1
+[ ! -x docker_api.sh ] && echo "docker_api.sh is dose not exist or is not executable" && exit 1
 
 . ./docker_api.sh
 
 for stage in [0-9][0-9][0-9]-*
 do
-	[ ! -x $stage/build.sh ] && exit 1
+	[ ! -x $stage/Dockerfile.sh ] && echo "$stage/Dockerfile.sh is dose not exist or is not executable" && exit 1
 	cd $stage
-	STAGE=$(grep "^\s*STAGE=" ./build.sh | cut -d= -f2-)
+	STAGE=$(grep "^\s*STAGE=" ./Dockerfile.sh | cut -d= -f2-)
+	[ ! -z SRAGE ] && echo "STAGE variable not set" && exit 1
+	echo "starting stage $STAGE"
 	./Dockerfile.sh
+	SQUASH
 	_ENDSTAGE
 	LASTSTAGE=${STAGE}
-
 	cd -
 done
-
-exit $rc

619e1457f17885369fdb4584d97c942c32a8f8c0
Willy Failla <wf@travelping.com>
2014-10-22 10:47:52 +0200

    add logging mechanism for docker stdout

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 [ ! -x build.sh ] && echo "build.sh is dose not exist or is not executable" && exit 1
 [ ! -x docker_api.sh ] && echo "docker_api.sh is dose not exist or is not executable" && exit 1
+mkdir -pv ./build-logs
 
 . ./docker_api.sh
 
@@ -17,7 +18,7 @@ do
 	cd $STAGE
 	export LOCATION=$(pwd)
 	[ -n "$LASTSTAGE" ] && export PREVIOUS_STAGE=${BUILDID}-${LASTSTAGE}
-	./Dockerfile.sh
+	./Dockerfile.sh | tee ../build-logs/${BUILDID}-${STAGE}.log
 	SQUASH
 	_ENDSTAGE
 	)

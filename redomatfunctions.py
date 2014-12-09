0e3b68e0282131810a8a4e67642fffd056235b8c
Tobias Hintze <thintze+git@tpip.net>
2014-12-09 09:55:06 +0100

    s/cli/client/ and fixes

--- redomatfunctions.py
+++ redomatfunctions.py
@@ -6,14 +6,14 @@ class Redomat:
 	def __init__(self,client=None):
 		if client is None:
 			raise Exception("client is not set")
-		self.cli = client
+		self.client = client
 		self.current_image = "test3"
 
 	def FROM(self, image=None):
 		if image is None:
 			raise Exception("no image given to work with")
 		print(str(image))
-		self.cli.tag(image,self.current_image)
+		self.client.tag(image,self.current_image)
 
 	def STAGE(self, text=None):
 		if text is None:
@@ -21,12 +21,12 @@ class Redomat:
 		print(text)
 
 	def RUN(self, cmd=None):
-		if self.cli is None:
+		if self.client is None:
 			raise Exception("No client given to work with")
 		if cmd is None:
 			raise Exception("RUN needs atleast one commadn")
-		self.cli.create_container(image=img, name='run-container', command=cmd)
-	#	cli.start(
+		self.client.create_container(image=self.current_image, name='run-container', command=cmd)
+	#	client.start(
 
 #FROM('ubuntu:14.04',client)
 #client.tag('ubuntu:14.04', 'test')

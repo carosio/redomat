d5ce87ec3e6d09bbf53714f78a73c8f974d0072b
Willy Failla <wf@travelping.com>
2014-12-12 18:09:36 +0100

    WIP

--- manifests/read_xml.py
+++ manifests/read_xml.py
@@ -1,33 +1,25 @@
 import xml.etree.ElementTree as XML
 
 manifest_root = XML.parse('tposs.xml').getroot()
-build_cmd = {}
-
 stages = []
 
 for buildstage in manifest_root.iter('buildstage'):
-	cmd = ""
-	target = ""
-	stage = {'id': buildstage.get('id')}
+	stage = {'id': buildstage.get('id'),
+		'dockerlines' : [],
+		'bitbake_target' : {'cmd' : [], 'target' : [] }}
 	stages.append(stage)
 
 	for stage_command in buildstage.iter():
-		if stage_command == 'dockerline':
-			stage['commands'].append({...})
 
-		if bitbake_target.get('command'):
-			cmd += bitbake_target.get('command') + " "
-		target += bitbake_target.text + " "
-		stage['cmd'] = cmd + target
+		if stage_command.tag == 'dockerline':
+			stage['dockerlines'].append(stage_command.text)
 
+		if stage_command.tag == 'bitbake_target':
 
-# xml find all buildstages and buildcommands etc.
-#for buildstage in manifest_root.findall('buildstage'):
-#	build_cmd.update({buildstage.get('id'):None})
-#	BS = build
-#	for cmd in buildstage.findall('bitbake_target'):
-#		text += cmd.get('command') + " "
-#	for cmd in buildstage.findall('bitbake_target'):
-#		text += cmd.text + " "
-#	build_cmd.update({buildstage.get('id') : text})
+			if stage_command.get('command'):
+				stage['dockerlines'].append(stage_command.get('command'))
+				stage['dockerlines'].append(stage['dockerlines'].pop() + " " + stage_command.text)
+			else:
+				stage['dockerlines'].append(stage_command.text)
+
 print("\n".join(map(lambda x: str(x), stages)))

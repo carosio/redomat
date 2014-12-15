c94960d22987127296382935741d81fad8231a16
Willy Failla <wf@travelping.com>
2014-12-15 10:20:15 +0100

    integrating the xml parser into the redomat

--- redomat.py
+++ redomat.py
@@ -2,7 +2,32 @@
 import docker
 import sys
 import os
+import xml.etree.ElementTree as XML
 from redomatfunctions import Redomat
+
+def xml_parser(xml_file):
+	manifest_root = XML.parse(xml_file).getroot()
+	stages = []
+
+	for buildstage in manifest_root.iter('buildstage'):
+		stage = {'id': buildstage.get('id'),
+			'dockerlines' : [],
+			'bitbake_target' : {'cmd' : [], 'target' : [] }}
+		stages.append(stage)
+
+		for stage_command in buildstage.iter():
+
+			if stage_command.tag == 'dockerline':
+				stage['dockerlines'].append(stage_command.text)
+
+			if stage_command.tag == 'bitbake_target':
+				stage['dockerlines'].append('RUN bitbake ')
+				if stage_command.get('command'):
+					stage['dockerlines'].append(stage['dockerlines'].pop() + '-c ' + stage_command.get('command'))
+					stage['dockerlines'].append(stage['dockerlines'].pop() + " " + stage_command.text)
+				else:
+					stage['dockerlines'].append(stage['dockerlines'].pop() + stage_command.text)
+	return stages
 
 def data_parser(docker_line, redo):
 	docker_command = docker_line.split(" ")
@@ -18,13 +43,13 @@ for dockerfile in sys.argv[1:]:
 		raise Exception("No Dockerfile found")
 		sys.exit(1)
 
-	inputfile = open(dockerfile)
+	stages=xml_parser(dockerfile)
 
 	redo = Redomat(docker.Client(base_url='unix://var/run/docker.sock',version='0.6.0'))
-	for line in inputfile:
-		if line.strip() == "":
-			continue
-		if line.strip().startswith("#"):
-			continue
-		data_parser(line.strip(), redo)
-	inputfile.close()
+
+	for stage in stages:
+		print(stage['id'])
+		for line in stage['dockerlines']:
+			print(line)
+			data_parser(line, redo)
+		print()

487201b255861b81b1830cc78ea75a4f933aa6a3
Willy Failla <wf@travelping.com>
2014-12-17 12:46:11 +0100

    fix add

--- redomatfunctions.py
+++ redomatfunctions.py
@@ -26,9 +26,10 @@ class Redomat:
 		"""
 			tag an image to begin from
 		"""
-		self.current_image="%s-%s-%s"%(time.strftime("%F-%H%M%S"), os.getenv('LOGNAME'), self.current_stage)
 		if image is None:
 			raise Exception("no image given to work with")
+
+		self.current_image="%s-%s-%s"%(time.strftime("%F-%H%M%S"), os.getenv('LOGNAME'), self.current_stage)
 		self.client.tag(image,self.current_image)
 
 	def RUN(self, cmd=None):
@@ -39,39 +40,52 @@ class Redomat:
 			raise Exception("No client given to work with")
 		if cmd is None:
 			raise Exception("RUN needs atleast one comman")
+
 		name = "%s-%s-%s"%(self.build_id, self.current_stage, self._nextseq())
 		self.client.create_container(image=self.current_image, name=name, command=cmd)
 		self.client.start(container=name, privileged=True)
+
 		if self.client.wait(container=name) is not 0:
 			raise Exception("Container " + name + " exited with a non zero exit status")
 		self.client.commit(container=name, repository=self.current_image)
 
 	def ADD(self, parameter=None):
-		""""
+		"""
 			ADD a file to an image
 		"""
 		if parameter is None:
 			raise Exception("No parameter given")
-		file_name, target_dir =parameter.split()
+		file_name, target =parameter.split()
 		file_name=self.current_stage + "/" + file_name
 		if file_name is None:
 			raise Exception("No filename given")
-		if target_dir is None:
+		if target is None:
 			raise Exception("No target directory given")
 		if os.path.exists(file_name) is False:
 			raise Exception("No such file: " + file_name)
+
 		file_name=os.path.basename(file_name)
-		print(file_name)
 		volume_path=os.path.abspath(self.current_stage)
-		print(volume_path)
-		name = "%s-%s-%s"%(self.build_id, self.current_stage, self._nextseq())
-		self.client.create_container(image=self.current_image, name=name, volumes=volume_path, command="mkdir -p " + target_dir  + " && cp -rv /files/" + file_name + " " + target_dir)
+		name = "%s-%s-%s-%s"%(self.build_id, self.current_stage, self._nextseq(), "create-target_dir")
+
+		self.client.create_container(image=self.current_image, name=name, command="/bin/mkdir -pv " + os.path.dirname(target))
+		self.client.start(container=name)
+		if self.client.wait(container=name) is not 0:
+                         raise Exception("Container " + name + " could not create a dir to use for th ADD command")
+		self.client.commit(container=name, repository=self.current_image)
+
+		name = "%s-%s-%s-%s"%(self.build_id, self.current_stage, self._nextseq(), "copy-data")
+
+		self.client.create_container(image=self.current_image, name=name, volumes=volume_path, command="cp -rv /files/" + file_name + " " + target)
 		self.client.start(container=name, binds={
-				'/files/':
+				volume_path:
 					{
-						'bind': volume_path,
-						'ro':True
+						'bind': '/files/',
+						'ro': True
 					}})
+
+		if self.client.wait(container=name) is not 0:
+			raise Exception("Container " + name + " exited with a non zero exit status")
 		self.client.commit(container=name, repository=self.current_image)
 
 	def WORKDIR(self, directory=None):
@@ -81,5 +95,8 @@ class Redomat:
 		if directory is None:
 			raise Exception("No directory given")
 		name = "%s-%s-%s"%(self.build_id, self.current_stage, self._nextseq())
-		delf.client.create_container(image=self.current_image, name=name, working_dir=dircetory)
+		self.client.create_container(image=self.current_image, name=name, working_dir=dircetory)
+
+		if self.client.wait(container=name) is not 0:
+			raise Exception("Container " + name + " exited with a non zero exit status")
 		self.client.commit(container=name, repository=self.current_image)

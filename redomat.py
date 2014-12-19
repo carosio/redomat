262a0e2bb55badb50f32554a308896df3f3966d0
Willy <wfailla@wfailla.de>
2014-12-19 15:22:52 +0100

    implement start_point option

--- redomat.py
+++ redomat.py
@@ -11,8 +11,8 @@ def xml_parser(xml_file):
 
 	for buildstage in manifest_root.iter('buildstage'):
 		stage = {'id': buildstage.get('id'),
-			'dockerlines' : [],
-			'bitbake_target' : {'cmd' : [], 'target' : [] }}
+			'build' : False,
+			'dockerlines' : []}
 		stages.append(stage)
 
 		for stage_command in buildstage.iter():
@@ -41,20 +41,37 @@ def data_parser(docker_line, redo):
 
 	callback(" ".join(docker_command[1:]).strip())
 
-for dockerfile in sys.argv[1:]:
+if sys.argv[1] == '-s':
+	start_number=3
+	start_point=sys.argv[2]
+	build=False
+else:
+	start_point=None
+	build=True
+	start_number=1
+
+for dockerfile in sys.argv[start_number:]:
 	if os.path.exists(dockerfile) is False:
 		raise Exception("No Dockerfile found")
 		sys.exit(1)
 
+	redo = Redomat(docker.Client(base_url='unix://var/run/docker.sock',version='0.6.0'))
+
 	stages=xml_parser(dockerfile)
 
-	redo = Redomat(docker.Client(base_url='unix://var/run/docker.sock',version='0.6.0'))
+	for stage in stages:
+		if stage['id'] == start_point:
+			build = True
+		if build==True:
+			stage['build']=True
 
 	for stage in stages:
 		print(stage['id'])
+		print(stage['build'])
 		redo.current_stage = stage['id']
 		for line in stage['dockerlines']:
-			print(line)
-			data_parser(line, redo)
+			if stage['build']:
+				print(line)
+				data_parser(line, redo)
 		redo.laststage = redo.current_image
 		print("done building: " + stage['id'])

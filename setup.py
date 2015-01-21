caf831d4e2ca04eafb6e640b7e28d94441e50d9b
Willy Failla <wfailla+git@tpip.net>
2015-01-21 10:36:41 +0100

    change setup.py so it will install libredo

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="redomat",
   version="0.1",
   install_requires=['docker'],
   data_files=[('/usr/bin', ['redomat.py'])],
-  packages=["redomatfunctions", "redomatfunctions.XML_parser","redomatfunctions.XML_creator"])
+  packages=["libredo", "libredo.XML_creator"])

f627b27cc77e6deb1c98d91b535e304a26bc451d
Willy Failla <wfailla+git@tpip.net>
2015-02-10 09:33:46 +0100

    remove the ConfCreator package from the install script since it is obsolete

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="redomat",
   version="0.1",
   install_requires=['docker'],
   data_files=[('/usr/bin', ['redomat.py'])],
-  packages=["libredo", "libredo.ConfCreator"])
+  packages=["libredo"])

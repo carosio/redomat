83d36a39d821fa7d0c227c27f1f132fb56b78ce1
Tobias Hintze <thintze+git@tpip.net>
2015-04-22 15:31:32 +0200

    fix basics-test

--- tests/basics.py
+++ tests/basics.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 
-import os, sys
 
-import libredo
+import readline,rlcompleter
+#!/usr/bin/env python
+
+import os, sys
 
 here = os.path.realpath(os.path.dirname(sys.argv[0]))
 print here
 
+print __file__
+sys.path.insert(0, here+"/..")
+
+import libredo
 
 d = libredo.Declaration()
-# FIXME copy example declaration into this repo
-d.parse("%s/example/example.xml"%here)
+d.parse("%s/../example/example.xml"%here)
 
 r = libredo.Redomat('unix://var/run/docker.sock')
-r.add(d)
 
-
-print d
-
-r.build('001-ubuntu')
+r.set_decl(d)
+r.set_dryrun(True)
+r.build('stage3')
 
